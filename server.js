9716cb3c332681fa4ca5516cc0fa8927e082dfd3
paradva-nilesh <paradvanilesh97@gmail.com>
2025-03-08 15:50:55 +0530

    Active And InActive

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(session({
     secret: 'Auth',
     resave: false,
     saveUninitialized: true,
+    cookie: { secure: false, maxAge : 1000 * 60 * 60 * 24 }
 }));
  
 app.use(passport.initialize());
